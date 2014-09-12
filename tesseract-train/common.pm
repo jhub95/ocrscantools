479aa224cc4d48a87ba8d3f8e9330e3d1cb8c734
Mark Zealey <mark@markandruth.co.uk>
2014-09-12 16:41:18 +0300

    regen

---
+++ tesseract-train/common.pm
@@ -0,0 +1,14 @@
+package common;
+use utf8;
+use strict;
+use warnings;
+
+sub is_allowed {
+    my ($w) = @_;
+    return 0 if $w =~ /[^!"\$%&'()*+\-\/0-9;<=>?\@A-Za-z\[\]_a-z,:. |«»ÂÇÔÎÖÛÜâçÊêîôöûüĞğİıŞş`“”\x{2018}\x{2019}]/;
+    return 0 if $w =~ /_{2,}/;
+    return 0 if $w =~ /\d/ and $w =~ /[^\d.,]/;
+    return 1;
+}
+
+1
