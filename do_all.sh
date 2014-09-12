6b55486be6f54eb13f0078b9b86a078a226c11f9
Mark Zealey <mark@markandruth.co.uk>
2014-09-12 21:06:58 +0300

    lock and sync with pi

--- do_all.sh
+++ do_all.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-cd /depo/scans
-for i in */; do
-    cd $i
-    /home/bookscanner/scantools/convert_book.pl pdf text #clean
-    if [ $? != 0 ]; then
-        pwd
-        echo "Command failed"
-    fi
-    cd ..
-done
+DIR=$(dirname $0)
+(
+    # Lock to make sure that only 1 process can run at the same time
+    flock -n 9 || exit 1
+    $DIR/sync_pi.sh
+
+    cd /depo/scans
+    for i in */; do
+        echo "About to process $i..."
+        cd $i
+        /home/bookscanner/scantools/convert_book.pl pdf text #clean
+        if [ $? != 0 ]; then
+            echo "Command failed: $i"
+        fi
+        cd ..
+    done
+
+    $DIR/sync_pi.sh
+) 9> /tmp/convert_book_lock
