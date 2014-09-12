43bf5ca605c767dbf3e6982558744b4084f76137
Mark Zealey <mark@markandruth.co.uk>
2014-09-12 17:47:30 +0300

    only pull raw from pi

--- sync_pi.sh
+++ sync_pi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 OPTS="-v -a --prune-empty-dirs"
 
-# Copy all files
-rsync $OPTS spreads@10.0.0.75:/media/kitaplar/scans/ /depo/scans/
+# Copy all files from pi - but only authoritative for the raw/ directories
+rsync $OPTS --include="*/" --include="*/raw/*" --exclude="**" spreads@10.0.0.75:/media/kitaplar/scans/ /depo/scans/
 
 # Copy all the book.conf and book output files over to the pi for backup purposes ignoring everything else
 rsync $OPTS --include="*/" --include="*/book.*" --exclude="**" /depo/scans/ spreads@10.0.0.75:/media/kitaplar/scans/
