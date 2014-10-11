35a130b4d9413315c3b9ab142b5fc4de3959c386
Mark Zealey <mark@markandruth.co.uk>
2014-10-11 12:46:36 +0300

    pi update

--- sync_pi.sh
+++ sync_pi.sh
@@ -5,9 +5,10 @@ OPTS="-v -a --prune-empty-dirs --update"
 rsync $OPTS --include="*/" --include="*/raw/*" --include="book.conf" --include="front.jpg" --include="back.jpg" --exclude="**" spreads@10.0.0.75:/media/kitaplar/scans/ /depo/scans/
 
 # Copy all the book.conf and book output files over to the pi for backup purposes ignoring everything else
-rsync $OPTS --include="*/" --include="*/book.*" --exclude="**" /depo/scans/ spreads@10.0.0.75:/media/kitaplar/scans/
+rsync $OPTS --include="*/" --include="*/book*" --include="front.jpg" --include="back.jpg" --exclude="**" /depo/scans/ spreads@10.0.0.75:/media/kitaplar/scans/
 
 for i in /depo/scans/*; do
+    # ignore blank ones
     if [[ ! -f "$i/book.conf" && -f "$i/raw/010.jpg" ]]; then
         cp /home/bookscanner/scantools/book.conf.default "$i/book.conf"
     fi
